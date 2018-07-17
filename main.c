2eea9a467285321ea3e97d2a72a511224af26824
wajd <wajdjtohme@gmail.com>
2018-07-17 09:09:33 +0200

    make_entity, toggle_entity

--- main.c
+++ main.c
@@ -1,97 +1,113 @@
 #include "header.h"
 
-// struct entity {
-// 	int x;
-// 	int y;
-// 	int width;
-// 	int height;
-// 	int on;
-// };
-//
-// struct bullet {
-// 	int x;
-// 	int y;
-// 	int dir;
-// 	int on;
-// };
-//
+struct entity {
+	int x;
+	int y;
+	int on;
+	int height;
+	int width;
+};
+
+struct bullet {
+	int x;
+	int y;
+	int on;
+	int dir;
+};
+
 // int updated = 0;
 
-//
-// struct entity make_spaceship (int x, int y, int width, int height) {
-// 	struct entity temp;
-// 	temp.x = x;
-// 	temp.y = y;
-// 	temp.width = width;
-// 	temp.height = height;
-// 	temp.on = 0;
-// 	//TODO: add checks for validity
-// 	return temp;
-// }
-//
-// void toggle_entity (struct entity ent) {
-// 	int i = ent.x - ent.width;
-// 	int j = ent.y - ent.height;
-//
-// 	if (i >= 0 && (i + 2*ent.width) < 128 && j >= 0 && (j + 2*ent.height) < 32) {
-// 		for (; i <= ent.x + ent.width; i++) {
-// 			for (j = ent.y - ent.height; j <= ent.y + ent.width; j++) {
-// 					toggle_pixel(&i, &j, ent.on);
-// 			}
-// 		}
-// 	}
-// 	updated = 1;
-// }
+
+struct entity make_entity (int x, int y, int height, int width) {
+	struct entity temp;
+	temp.x = x;
+	temp.y = y;
+	temp.height = height;
+	temp.width = width;
+	temp.on = 1;
+	//TODO: add checks for validity
+	return temp;
+}
+
+void toggle_entity (struct entity ent) {
+	int i, j, x, y;
+	x = ent.x - ent.width;
+	y = ent.y - ent.height;
+
+	if (x >= 0 && (x + 2*ent.height) < 128 && y >= 0 && (y + 2*ent.width) < 32) {
+		for (j = ent.y - ent.width; j <= ent.y + ent.width; j++) {
+			for (i = ent.x - ent.height; i <= ent.x + ent.height; i++) {
+				x = i;
+				y = j;
+				toggle_pixel(&x, &y, ent.on);
+			}
+		}
+	}
+}
 
 void test() {
 
-	// struct entity spaceship = make_spaceship(15, 16, 6, 8);
-	// toggle_entity(spaceship);
+	struct entity spaceship = make_entity(15, 16, 3, 2);
 
-	/*current testing routine*/
-	int x, y, px, py;
+	// int x, y, px, py;
+	//
+	// int pressed = 0;
+	//
+	// px = 60;
+	// py = 16;
+	// x = 60;
+	// y = 16;
+	//int i, j, x, y;
 
-	int pressed = 0;
-
-	px = 60;
-	py = 16;
-	x = 60;
-	y = 16;
 	for(;;) {
-
-		if (getbtn(1)) {
-			x++;
-			pressed = 1;
-		}
-		else if (getbtn(4)) {
-			x--;
-			pressed = 1;
-		}
-
-		if (getbtn(2)) {
-			y++;
-			pressed = 1;
-		}
-		else if (getbtn(3)) {
-			y--;
-			pressed = 1;
-		}
-
-		if (pressed) {
-			toggle_pixel(&px, &py, 0);
-			toggle_pixel(&x, &y, 1);
-			px = x;
-			py = y;
-
+		if (getbtn(4)) {
+			toggle_entity(spaceship);
 			display_update();
-			pressed = 0;
 		}
+		// if (getbtn(1)) {
+		// 	for (j = spaceship.y - spaceship.width; j <= spaceship.y + spaceship.width; j++) {
+		// 		for (i = spaceship.x - spaceship.height; i <= spaceship.x + spaceship.height; i++) {
+		// 			// x = i;
+		// 			// y = j;
+		// 			toggle_pixel(&i, &j, spaceship.on);
+		// 		}
+		// 	}
+		// 	display_update();
+		// }
+		// if (getbtn(1)) {
+		// 	x++;
+		// 	pressed = 1;
+		// }
+		// else if (getbtn(4)) {
+		// 	x--;
+		// 	pressed = 1;
+		// }
+		//
+		// if (getbtn(2)) {
+		// 	y++;
+		// 	pressed = 1;
+		// }
+		// else if (getbtn(3)) {
+		// 	y--;
+		// 	pressed = 1;
+		// }
+		//
+		// if (pressed) {
+		// 	toggle_pixel(&px, &py, 0);
+		// 	toggle_pixel(&x, &y, 1);
+		// 	px = x;
+		// 	py = y;
+		//
+		// 	display_update();
+		// 	pressed = 0;
+		// }
 	}
 }
 
 int main() {
 	setup();
 
+	/*current testing routine*/
 	test();
 
 	for(;;) ;
